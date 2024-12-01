34c8a66d063005a3a0342d5022836646bfd73b7f
Peter Pham <pttp15122002@gmail.com>
2024-12-01 10:12:36 +0700

    add colors and animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2dc653",
+        },
+        secondary: "#ffc43d",
+      },
+      animation: {
+        wave: "wave 1s infinite",
+      },
+      keyframes: {
+        wave: {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "50%": { transform: "rotate(25deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
