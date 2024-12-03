6f04b77f35267eb1bba68b9e83015eda966b29dc
Peter Pham <pttp15122002@gmail.com>
2024-12-03 08:29:07 +0700

    fix: animation does not work

--- src/components/LatestProduct.tsx
+++ src/components/LatestProduct.tsx
@@ -3,7 +3,7 @@ import { useInView } from "motion/react";
 
 export default function LatestProduct() {
   const ref = useRef(null);
-  const isLatestProductInView = useInView(ref, { once: true });
+  const isLatestProductInView = useInView(ref, { once: true, amount: 0.5 });
 
   return (
     <section ref={ref}>
@@ -11,25 +11,19 @@ export default function LatestProduct() {
         <h1 className="md:text-xl lg:text-2xl font-bold">
           My <span className="text-secondary">Latest Product</span>
         </h1>
-        <p className="font-bold text-4xl md:text-5xl lg:text-7xl">
-          <span
-            style={{
-              transform: isLatestProductInView ? "none" : "translateX(-200px)",
-              opacity: isLatestProductInView ? 1 : 0,
-              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-            }}
-          >
-            Introducing
-          </span>{" "}
+        <p
+          style={{
+            transform: isLatestProductInView ? "none" : "translateX(200px)",
+            opacity: isLatestProductInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0s",
+          }}
+          className="font-bold text-4xl md:text-5xl lg:text-7xl"
+        >
+          <span>Introducing</span>{" "}
           <a
             href="https://www.deviniter.site/"
             target="_blank"
             className="text-primary underline"
-            style={{
-              transform: isLatestProductInView ? "none" : "translateX(200px)",
-              opacity: isLatestProductInView ? 1 : 0,
-              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-            }}
           >
             DevIniter
           </a>
