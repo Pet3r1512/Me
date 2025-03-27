0e3b8c915980cba3da8ed10a1d6db2a4e170cbba
Peter Pham <pttp15122002@gmail.com>
2025-03-28 10:14:06 +1100

    fix: missing app.css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "./contexts/ThemeContext";
 import AnimatedBackground from "./components/Background";
 import Header from "./components/Layout/Header/_index";
+import "./App.css";
 
 export default function App() {
   return (
