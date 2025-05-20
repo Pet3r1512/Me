d4efacc6a72c871427f7a9958e78130382903567
Peter Pham <pttp15122002@gmail.com>
2025-05-20 19:26:07 +1000

    feat: proficiency section

---
+++ src/components/Sections/Tech/Proficiency.tsx
@@ -0,0 +1,40 @@
+export default function Proficiency() {
+  return (
+    <div className="space-y-5">
+      <p className="text-2xl md:text-3xl font-semibold text-green-600">
+        Proficiency
+      </p>
+      <ul className="space-y-1 list-disc mx-10">
+        <li>
+          <strong>Programming Languages: </strong>JavaScript, TypeScript
+        </li>
+        <li>
+          <strong>Front End: </strong>HTML5, CSS3(CSS-preprocessor: Sass),
+          ReactJS, TypeScript, TailwindCSS, Tanstack (Query, Router and Start)
+        </li>
+        <li>
+          <strong>Back End: </strong>NextJS (Page Router), ExpressJS, Hono, tRPC
+        </li>
+        <li>
+          <strong>Testing: </strong>Vitest, Jest
+        </li>
+        <li>
+          <strong>Databases and ORMs: </strong>NoSQL(MongoDB) and
+          SQL(PostgreSQL, MySQL), Prisma, Drizzle
+        </li>
+        <li>
+          <strong>State Management: </strong>Zustand
+        </li>
+        <li>
+          <strong>Hosting: </strong>Cloudflare, Vercel
+        </li>
+        <li>
+          <strong>CI/CD: </strong> CircleCI, Github Actions
+        </li>
+        <li>
+          <strong>Scripting: </strong>Bash
+        </li>
+      </ul>
+    </div>
+  );
+}
